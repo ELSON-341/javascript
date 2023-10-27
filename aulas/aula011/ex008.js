4943b448dc3a48a01af069b8f4d96a7e56f5cf3b
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2023-10-27 14:33:31 -0300

    criei pastas

---
+++ aulas/aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60) { // condião simples.
+    console.log('Você utrapassou velocidade permitida.MULTADO!')
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
---
+++ aulas/aula011/ex009.js
@@ -0,0 +1,15 @@
+// var país = 'Blasil'
+// console.log(`Vivendo em ${país}`)
+// if(país == 'Blasil') {
+//     console.log('Você é blasileiro!')
+// }
+// else {
+//     console.log('Você é estrangeira!')
+// }
+var num = 3
+if(num % 2 == 0) {
+    console.log(`O número ${num} é par`)
+}
+else {
+    console.log(`O número ${num} é ímpar`)
+}
