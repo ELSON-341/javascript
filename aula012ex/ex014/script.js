50d8a09a05e0df84efa13433e1f61ef3141a8449
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2023-10-28 10:54:33 -0300

    atualizei aula012ex

--- aula012ex/ex014/script.js
+++ aula012ex/ex014/script.js
@@ -1,1 +1,22 @@
-window.alert('ok')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('foto')
+    var ima = window.document.getElementsByTagName('img')[0]
+    var data = new Date()
+    var hora = data.getHours()
+    var hora = 10
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if(hora >= 0 && hora < 11) {
+        // BOM DIA!
+        ima.src = 'foto-manha.png'
+        window.document.body.style.background = '#e2cd9f'
+    } else if(hora >= 12 && hora < 18) { 
+        // BOA TARDE!
+        ima.src = 'foto-tarde.png'
+        window.document.body.style.background = '#68a5f3'
+    }
+    else {
+        // BOA NOITE!
+        ima.src = 'foto-noite.png'
+    }
+}
