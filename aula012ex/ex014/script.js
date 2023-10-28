b88f333492c000052d3ff34e8c16387ed7afc878
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2023-10-28 15:02:18 -0300

    colocando as imagens

--- aula012ex/ex014/script.js
+++ aula012ex/ex014/script.js
@@ -4,12 +4,11 @@ function carregar() {
     var ima = window.document.getElementsByTagName('img')[0]
     var data = new Date()
     var hora = data.getHours()
-    var hora = 10
     msg.innerHTML = `Agora são ${hora} horas.`
     if(hora >= 0 && hora < 11) {
         // BOM DIA!
         ima.src = 'foto-manha.png'
-        window.document.body.style.background = '#e2cd9f'
+        window.document.body.style.background = '#f5d08c'
     } else if(hora >= 12 && hora < 18) { 
         // BOA TARDE!
         ima.src = 'foto-tarde.png'
@@ -18,5 +17,6 @@ function carregar() {
     else {
         // BOA NOITE!
         ima.src = 'foto-noite.png'
+        window.document.body.style.background = '#707099'
     }
 }
