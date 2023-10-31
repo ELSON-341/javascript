5c2f08fa8575d209e1d25bddb37ab11e742d1a49
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2023-10-31 15:21:06 -0300

    lasasos de repetição

--- aula013/anbiente.js
+++ aula013/anbiente.js
@@ -1,5 +1,13 @@
-var c = 1
-while(c <= 10) {
-    console.log(`posso ${c}`)
-    c++
+// var c = 1
+// while(c <= 10) {
+//     console.log(`posso ${c}`)
+//     c++
+// } 
+// var c = 1
+// do {
+//     console.log(`Passo ${c}`)
+//     c++
+// } while(c <= 10)
+for(var c = 1; c <= 30; c++) {
+    console.log(`Passo ${c}`)
 }
