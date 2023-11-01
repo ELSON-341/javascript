0c4a478eb4d39a88f7fd270b15c711742f9f6165
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2023-11-01 15:31:56 -0300

    tentado cria aula014ex

--- aula013/anbiente.js
+++ aula013/anbiente.js
@@ -1,13 +1,5 @@
-// var c = 1
-// while(c <= 10) {
-//     console.log(`posso ${c}`)
-//     c++
-// } 
-// var c = 1
-// do {
-//     console.log(`Passo ${c}`)
-//     c++
-// } while(c <= 10)
-for(var c = 1; c <= 30; c++) {
-    console.log(`Passo ${c}`)
-}
+console.log('Vai começar...')
+for(var c = 1; c <= 5; c++) {
+    console.log(c)
+} 
+console.log('FIM!')
---
+++ aula014ex/modelo/script.js
@@ -0,0 +1,23 @@
+/* [X] validação da caixa vazia 
+   [X] validação do passo 0*/
+var bot = window.document.getElementById('botao')
+bot.addEventListener('click', contagem)
+function contagem() {
+    var tinicio = window.document.getElementById('txt_inicio')
+    var tfim = window.document.getElementById('txt_fim')
+    var res = window.document.querySelector('div#res')
+    var tpasso = window.document.querySelector('input#txt_passo')
+    if(tinicio.value.length == 0 || tfim.value.length == 0 || tpasso.value.length == 0) {
+        res.innerHTML = 'Inpossível contar!'
+    } else if(Number(tpasso.value) == 0) {
+        tpasso = 1
+        window.alert('Passo inválido! Considerando PASSO 1.')
+    } else {
+        res.innerHTML = 'Contando:<br>'
+        var p = tinicio
+        while(p <= tfim) {
+            res.innerHTML = `${p}`
+            p + tpasso
+        }
+    }   
+}
