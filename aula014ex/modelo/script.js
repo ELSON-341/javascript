b24d601f8968b7bd847ac12b315faa044930e47d
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2023-11-03 14:22:05 -0300

    exercício 014 concluído

--- aula014ex/modelo/script.js
+++ aula014ex/modelo/script.js
@@ -4,14 +4,25 @@ function contador() {
     let pas = window.document.getElementById('txt_pas')
     let res = window.document.getElementById('res')
     if(ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
-        window.alert('[ERRO] Faltan dados!')
+        res.innerHTML = 'Impossível contador!'
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(pas.value)
-        for(let c = i; c <= f; c += p) {
-            res.innerHTML += `${c} `
+        if(p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1.')
+            p = 1
         }
+        if(i < f) {
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += '\u{1F3C1}'
     }
 }
