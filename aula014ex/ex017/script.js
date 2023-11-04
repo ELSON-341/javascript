2b1a6b824c7bd158b220e8fded7d743467212590
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2023-11-04 13:55:28 -0300

    corrigido o ex017

--- aula014ex/ex017/script.js
+++ aula014ex/ex017/script.js
@@ -4,11 +4,12 @@ function tabuada() {
     if(tnum.value.length == 0) {
         window.alert('Por favor digite um número!')
     } else {
+        res.innerHTML = ""
         let num = Number(tnum.value)
         let c = 1
         while(c <= 10) {
             let item = window.document.createElement('option')
-            item.text = `${num} X ${c} = ${num * c}`
+            item.text = `${num} X ${c} = ${num * c.toFixed(2)}`
             res.value = `${c}`
             res.appendChild(item)
             c++
