8f32e1a2b11653c5492bfbf7bf5fd7544ce6d4da
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2026-05-16 15:12:18 -0300

    update

--- aula011/ex008.js
+++ aula011/ex008.js
@@ -1,6 +1,1 @@
-var vel = 12
-console.log(`A velocidade do seu carro é ${vel}km/h`)
-if(vel > 60) { // condião simples.
-    console.log('Você utrapassou velocidade permitida.MULTADO!')
-}
-console.log(`Dirija sempre usando o cinto de segurança!`)
+const vel = 60.5
