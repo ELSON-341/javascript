8cebd9c3953a104d7260655036833c7f9567fa5b
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2026-05-30 14:35:54 -0300

    Anorther update

--- aula012ex/ex014/script.js
+++ aula012ex/ex014/script.js
@@ -1,23 +1,28 @@
 const p = document.getElementsByTagName('p')[0]
 const img = document.getElementById('photo')
 
-// const now = new Date()
-// const hour = now.getHours()
+function hours() {
+    const now = new Date()
+    const hour = now.getHours()
+    const minutes = now.getMinutes()
+    const seconds = now.getSeconds()
 
-const hour = 17
-p.innerText = `Agora são ${hour} horas`
+    // const hour = 17
+    p.innerText = `Agora são ${hour} horas e ${minutes} minutos e ${seconds} segundos`
 
 
-if (hour < 12) {
-    // MORNING
-    img.src = 'foto-manha.png'
-    document.body.style.background = '#f5d08c'
-} else if (hour < 18) {
-    // AFTERNOON
-    img.src = 'foto-tarde.png'
-    document.body.style.background = '#68a5f3'
-} else {
-    // NIGHT
-    img.src = 'foto-noite.png'
-    document.body.style.background = '#707099'
+    if (hour < 12) {
+        // MORNING
+        img.src = 'foto-manha.png'
+        document.body.style.background = '#f5d08c'
+    } else if (hour < 18) {
+        // AFTERNOON
+        img.src = 'foto-tarde.png'
+        document.body.style.background = '#68a5f3'
+    } else {
+        // NIGHT
+        img.src = 'foto-noite.png'
+        document.body.style.background = '#707099'
+    }
+
 }
