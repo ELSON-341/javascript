ab8ee905840c0d4159dcc83ca96ec49437f304d2
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2026-05-31 10:43:00 -0300

    update

--- aula012ex/ex015/script.js
+++ aula012ex/ex015/script.js
@@ -4,11 +4,21 @@ function toCheck() {
     const fYaer = document.getElementById('txtano')
     const res = document.querySelector('div#res')
 
+
     if (fYaer.value.length == 0 || fYaer.value > yaer) {
         window.alert("[ERRO] Verifique os dados e tente novamente!")
-    } else {
-        window.alert('Tudo ok')
-    }
+        return
+    } 
 
+    const redSex = window.document.getElementsByName('radsex')
+    const age = yaer - Number.parseInt(fYaer.value)
+    let gender = ''
     
+    if (redSex[0].checked) {
+        gender = 'Homem'
+    } else {
+        gender = 'Mulher'
+    }
+    res.style.textAlign = 'center'
+    res.innerHTML = `Você é ${gender}`
 }
