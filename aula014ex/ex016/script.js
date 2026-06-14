73cdb2613826b597edca987f02eeaa5cae038057
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2026-06-14 10:12:26 -0300

    update

--- aula014ex/ex016/script.js
+++ aula014ex/ex016/script.js
@@ -1,28 +1,32 @@
-function contador() {
-    let ini = window.document.getElementById('txt_ini')
-    let fim = window.document.getElementById('txt_fim')
-    let pas = window.document.getElementById('txt_pas')
-    let res = window.document.getElementById('res')
-    if(ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
-        res.innerHTML = 'Impossível contador!'
-    } else {
-        res.innerHTML = 'Contando: <br>'
-        let i = Number(ini.value)
-        let f = Number(fim.value)
-        let p = Number(pas.value)
-        if(p <= 0) {
-            window.alert('Passo inválido! Considerando PASSO 1.')
-            p = 1
-        }
-        if(i < f) {
-            for(let c = i; c <= f; c += p) {
-                res.innerHTML += `${c} \u{1F449}`
-            }
-        } else {
-            for(let c = i; c >= f; c -= p) {
-                res.innerHTML += `${c} \u{1F449}`
-            }
-        }
-        res.innerHTML += '\u{1F3C1}'
+const counter = () => {
+    const ini = window.document.getElementById('txt-ini')
+    const fim = window.document.getElementById('txt-fim')
+    const pas = window.document.getElementById('txt-pas')
+    const res = window.document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
+        window.alert('Impossível contador!')
+        return
     }
+
+    res.innerHTML = 'Contando: <br>'
+    const i = Number(ini.value)
+    const f = Number(fim.value)
+    let p = Number(pas.value)
+
+    if (p <= 0) {
+        window.alert('Passo inválido! Considerando PASSO 1.')
+        p = 1
+    }
+    
+    if (i < f) {
+        for (let c = i; c <= f; c += p) {
+            res.innerHTML += `${c} \u{1F449}`
+        }
+    } else {
+        for (let c = i; c >= f; c -= p) {
+            res.innerHTML += `${c} \u{1F449}`
+        }
+    }
+    res.innerHTML += '\u{1F3C1}'
 }
