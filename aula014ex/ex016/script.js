42328fee926363e81703986ccc77614284b41643
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2026-06-14 14:12:41 -0300

    update

--- aula014ex/ex016/script.js
+++ aula014ex/ex016/script.js
@@ -5,7 +5,8 @@ const counter = () => {
     const res = window.document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
-        window.alert('Impossível contador!')
+        res.innerHTML = 'Impossível contar!'
+        window.alert('[ERRO] Faltam dados')
         return
     }
 
@@ -20,13 +21,15 @@ const counter = () => {
     }
     
     if (i < f) {
+        // count-up
         for (let c = i; c <= f; c += p) {
-            res.innerHTML += `${c} \u{1F449}`
+            res.innerHTML += ` ${c} \u{1F449}`
         }
     } else {
+        // countdonw
         for (let c = i; c >= f; c -= p) {
-            res.innerHTML += `${c} \u{1F449}`
+            res.innerHTML += `${c} \u{1F448}`
         }
     }
-    res.innerHTML += '\u{1F3C1}'
+    res.innerHTML += ' \u{1F3C1}'
 }
