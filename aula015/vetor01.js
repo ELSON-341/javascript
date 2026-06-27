ace0332a99895d0a7330735a01cd973434983da4
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2026-06-27 15:46:11 -0300

    vetor

--- aula015/vetor01.js
+++ aula015/vetor01.js
@@ -1,10 +1,12 @@
-let num = [8, 4, 9]
-num[3] = 5
-num.push(7)
+let num = [5, 8, 2, 9, 3]
+num.push(1)
 num.sort()
-num.indexOf()
-for(let c = 0; c < num.length; c++){
-    console.log(`A posição ${c} tem o valor ${num[c]}`)
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`) 
+let pos = num.indexOf(4)
+if (pos == -1) {
+    console.log(`O valor não foi encontrado!`)
+} else {
+    console.log(`O valor 4 está na posição ${pos}`)
 }
-// console.log(num.length)
-// console.log(`${num}`)
